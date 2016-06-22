22c3341b79218cdbda36c59d2338b120d3daa74c
Joachim Wester <joachimwester@mac.com>
2016-06-22 11:38:21 +0200

    Added Marcus Zetterquist Persistent Vector Trie implementation as starting point for immutable vector list

---
+++ src/Optimized_Vector.cpp
@@ -0,0 +1,15 @@
+// This code is optional. It will only be used if you compile with USE_OPTIMIZATIONS.
+// Skip this when you first want to learn about working with the Addie codebase.
+//
+// Created by Joachim Wester on 22/04/16.
+// Copyright © 2016 Joachim Wester, Starcounter AB.
+//
+// Parts Copyright 2015 Marcus Zetterquist under the Apache license
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+
+#include "Optimized_Vector.hpp"
+
+
+
+
